d4a5c411765365e3183a655f5d5f55d62f482224
George <77338393+papageo21@users.noreply.github.com>
2022-01-03 17:21:48 +0200

    Create func.lua

---
+++ Calculator/func.lua
@@ -0,0 +1,21 @@
+funcmathf = {}
+
+function funcmathf.add(num1, num2)  
+    return num1 + num2
+end    
+function funcmathf.sub(num1, num2)
+    return num1 - num2
+end
+function funcmathf.pro(num1, num2)
+    return num1 * num2
+end
+function funcmathf.div(num1, num2)
+    return num1 / num2
+end
+function funcmathf.power(num1, num2)
+    return num1 ^ num2
+end
+function funcmathf.id(num1, num2)
+    return num1 % num2
+end
+return funcmathf
